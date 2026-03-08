6d0152804d03d8f08a88036b7de305c5f15ec671
luchamediapro <entretenimientogaran@gmail.com>
2026-03-08 11:26:07 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 const BASE = "http://45.225.68.1:8532/Live/878e0987f8fffce401028e0283b0b24d/";
 
+app.get("/", (req,res)=>{
+    res.send("Proxy HLS activo");
+});
+
 app.get("/stream", async (req,res)=>{
 
     const file = req.query.file || "local-ch30.playlist.m3u8";
@@ -12,22 +16,26 @@ app.get("/stream", async (req,res)=>{
 
     try{
 
+        // PLAYLIST
         if(file.includes(".m3u8")){
 
             const response = await axios.get(url,{
-                headers:{ "User-Agent":"Mozilla/5.0" }
+                headers:{
+                    "User-Agent":"Mozilla/5.0",
+                    "Connection":"keep-alive"
+                }
             });
 
             let playlist = response.data;
 
             const lines = playlist.split("\n");
 
-            const newPlaylist = lines.map(line => {
+            const newPlaylist = lines.map(line=>{
 
                 line = line.trim();
 
                 if(line.endsWith(".m3u8") || line.endsWith(".ts")){
-                    return "/stream?file=" + line;
+                    return "/stream?file=" + line + "&t=" + Date.now();
                 }
 
                 return line;
@@ -36,20 +44,26 @@ app.get("/stream", async (req,res)=>{
 
             res.setHeader("Content-Type","application/vnd.apple.mpegurl");
 
-            // 🔴 evitar cache
+            // evitar cache
             res.setHeader("Cache-Control","no-cache, no-store, must-revalidate");
             res.setHeader("Pragma","no-cache");
             res.setHeader("Expires","0");
 
             res.send(newPlaylist);
 
-        }else{
+        }
+
+        // SEGMENTOS
+        else{
 
             const stream = await axios({
                 url:url,
                 method:"GET",
                 responseType:"stream",
-                headers:{ "User-Agent":"Mozilla/5.0" }
+                headers:{
+                    "User-Agent":"Mozilla/5.0",
+                    "Connection":"keep-alive"
+                }
             });
 
             res.setHeader("Content-Type","video/mp2t");
@@ -60,8 +74,9 @@ app.get("/stream", async (req,res)=>{
 
     }catch(e){
 
-        console.log(e.message);
-        res.status(500).send("Error stream");
+        console.log("Error:", e.message);
+
+        res.status(500).send("Error cargando stream");
 
     }
 
@@ -71,6 +86,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
 
-    console.log("Proxy HLS activo");
+    console.log("Proxy HLS funcionando");
 
 });
